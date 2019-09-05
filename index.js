756c05c5c58faa1bb17f860426256ee8c33882d1
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-05 14:54:08 -0600

    Trying to get deploy to Heroku by changing the start scripts from package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 require('dotenv').config()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => console.log(`\n** API on port ${port}**\n`))
